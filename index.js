2bde7be12b5baeee4c97336fa89f06d624ddadc8
BTHS-BIGOLDTURKEY <zakariaal2@nycstudents.net>
2024-10-16 10:57:05 -0400

    done

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 //school counter
 let count = 0;
 let clicks = 0;
+let entries = []
+entrTxt = ""
 document.getElementById("count-el").innerText = count
 function increment(){
     count++
     clicks++
     document.getElementById("count-el").innerText = count
     console.log(clicks)
+}
+function save(){
+    entries.push(count)
+    entrTxt += count+" - "
+    document.getElementById("prev").innerText = entrTxt
 }
